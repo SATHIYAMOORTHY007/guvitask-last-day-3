5fc9a5520e2be0aae53dbe218f4b15fbbd395743
SATHIYA MOORTHY S <67869734+SATHIYAMOORTHY007@users.noreply.github.com>
2023-06-20 10:18:42 +0530

    Delete student backend directory

--- student backend/routes/student.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const { studentCreate, getAllStudent } = require('../controllers/student')
-
-router.get('/', getAllStudent)
-router.post('/create', studentCreate)
-
-module.exports = router
